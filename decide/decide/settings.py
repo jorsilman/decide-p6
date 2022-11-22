77d2a3f5b907f79b5599d5019ea7cca2deab91f9
Jorger Sillero <jorgesilleromanchon@gmail.com>
2022-11-22 12:02:32 +0100

    CC añade settings.py

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '^##ydkswfu0+=ofw0l#$kv^8n)0$i(qd&d&ol#p9!b$8*5%j1+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -70,7 +70,20 @@ MODULES = [
     'voting',
 ]
 
-BASEURL = 'http://localhost:8000'
+#BASEURL = 'http://localhost:8000'
+BASEURL = 'http://app-07cf1efb-8a62-41bb-94c1-a9658215b87c.cleverapps.io/'
+
+APIS = {
+    'authentication': BASEURL,
+    'base': BASEURL,
+    'booth': BASEURL,
+    'census': BASEURL,
+    'mixnet': BASEURL,
+    'postproc': BASEURL,
+    'store': BASEURL,
+    'visualizer': BASEURL,
+    'voting': BASEURL,
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -109,11 +122,11 @@ WSGI_APPLICATION = 'decide.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'decide',
-        'USER': 'decide',
-        'PASSWORD': 'decide',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': os.environ.get('POSTGRESQL_ADDON_DB'),
+        'USER': os.environ.get('POSTGRESQL_ADDON_USER'),
+        'PASSWORD': os.environ.get('POSTGRESQL_ADDON_PASSWORD'),
+        'HOST': os.environ.get('POSTGRESQL_ADDON_HOST'),
+        'PORT': os.environ.get('POSTGRESQL_ADDON_PORT'),
     }
 }
 
@@ -157,6 +170,8 @@ TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT= 'static/'
+
 
 # number of bits for the key, all auths should use the same number of bits
 KEYBITS = 256
